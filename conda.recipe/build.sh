c631648f9005a8a0b87b251775f3ce297f10933a
Thomas Saillour <saillour.thomas@gmail.com>
2023-07-18 10:27:36 +0200

    changed throughly the build.sh to copy the HOMETEL in the conda env

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -2,23 +2,50 @@
 
 set -xeuo pipefail
 
-#export mpi=mpich
-export CC=mpicc
-export CXX=mpicxx
-export FC=mpif90
-export F77=mpif77
-export F90=mpif90
-
-# build and install schism
-ls
-ls ..
+# build and install telemac
 source ./configs/pysource.template.sh
 compile_telemac.py
 
-cp -r $HOMETEL/builds/$USETELCFG/lib/* $PREFIX/bin
-mkdir $PREFIX/$USETELCFG/
-mkdir $PREFIX/$USETELCFG/wrap_api/
-mkdir $PREFIX/$USETELCFG/wrap_api/lib/
-cp -r $HOMETEL/builds/$USETELCFG/wrap_api/lib/* $PREFIX/$USETELCFG/wrap_api/lib/
+#1: the config folder 
+export CONFIGS=$PREFIX/configs/
+if [ -d "$CONFIGS" -a ! -h "$CONFIGS" ]
+then
+   echo "$CONFIGS already exists"
+else
+   mkdir $CONFIGS
+fi 
+
+#2: the builds folder 
+export BUILDS=$PREFIX/builds/
+if [ -d "$BUILDS" -a ! -h "$BUILDS" ]
+then
+   echo "$BUILDS already exists"
+else
+   mkdir $BUILDS
+fi 
+
+#3 the scripts folder 
+export SCRIPTS=$PREFIX/scripts/
+if [ -d "$SCRIPTS" -a ! -h "$SCRIPTS" ]
+then
+   echo "$SCRIPTS already exists"
+else
+   mkdir $SCRIPTS
+fi 
+
+#4 the source folder 
+export SOURCES=$PREFIX/sources/
+if [ -d "$SOURCES" -a ! -h "$SOURCES" ]
+then
+   echo "$SOURCES already exists"
+else
+   mkdir $SOURCES
+fi 
+
+cp -r $HOMETEL/configs/* $PREFIX/configs     #1
+cp -r $HOMETEL/builds/* $PREFIX/builds       #2
+cp -r $HOMETEL/scripts/python3/* $PREFIX/bin #3
+cp -r $HOMETEL/scripts/* $PREFIX/scripts #3
+cp -r $HOMETEL/sources/* $PREFIX/sources #4
 
 cd $BUILD_PREFIX
