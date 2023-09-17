2899f7586a7feaaaac49810011f4ee86638cfb48
nicogodet <nicolas.godet@outlook.fr>
2023-09-17 13:55:50 +0200

    Move examples and documentation to another repo

--- recipe/build-examples.bat
+++
@@ -1,8 +0,0 @@
-:: TELEMAC home directory
-set HOMETEL=%SRC_DIR%\telemac-mascaret
-
-:: Copy sources
-mkdir %LIBRARY_PREFIX%\telemac-mascaret\examples
-mkdir %LIBRARY_PREFIX%\telemac-mascaret\notebooks
-xcopy %HOMETEL%\examples %LIBRARY_PREFIX%\telemac-mascaret\examples /E /H /C /I
-xcopy %HOMETEL%\notebooks %LIBRARY_PREFIX%\telemac-mascaret\notebooks /E /H /C /I
