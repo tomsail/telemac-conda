c299a847b82bd9024733257e89862300af1f5523
nicogodet <nicolas.godet@outlook.fr>
2023-09-17 14:39:50 +0200

    Run some tests

---
+++ recipe/run_test.bat
@@ -0,0 +1,3 @@
+:: Tests
+python -m validate_telemac --bypass --tags=telemac2d,telemac3d,gaia,tomawac,-mascaret,fv,nestor,waqtel,python3,apistudy,-api_mascaret,coupling,postel3d,stbtel,khione,api,artemis,-med,-aed,-gotm --api --rank 0
+python -m validate_telemac --clean
