69633ce5dc8b80fa7558a8fb71be1add5a3768ab
Thomas Saillour <saillour.thomas@gmail.com>
2023-10-06 09:26:48 +0200

    added Linux and macOs support

---
+++ recipe/build-examples.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# TELEMAC home directory
+export HOMETEL=$SRC_DIR/telemac-mascaret
+# Copy sources
+
+#5 the examples and notebooks folder
+export EXAMPLES=$PREFIX/examples/
+if [ -d "$EXAMPLES" -a ! -h "$EXAMPLES" ]
+then
+   echo "$EXAMPLES already exists"
+else
+   mkdir $EXAMPLES
+fi
+
+export NOTEBOOKS=$PREFIX/notebooks/
+if [ -d "$NOTEBOOKS" -a ! -h "$NOTEBOOKS" ]
+then
+   echo "$NOTEBOOKS already exists"
+else
+   mkdir $NOTEBOOKS
+fi
+
+cp -r $HOMETEL/examples/* $PREFIX/examples     #5
+cp -r $HOMETEL/notebooks/* $PREFIX/notebooks   #5
