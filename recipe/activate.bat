3026fa5e517ae3ebaad1a848e416ad936690d08d
nicogodet <nicolas.godet@outlook.fr>
2023-11-01 18:21:23 +0100

    No function in bat

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -12,18 +12,3 @@ set PATH=%HOMETEL%\scripts\python3;%PATH%
 set PYTHONPATH=%HOMETEL%\scripts\python3;%PYTHONPATH%
 set PYTHONPATH=%HOMETEL%\builds\%USETELCFG%\wrap_api\lib;%PYTHONPATH%
 set PYTHONUNBUFFERED="true"
-
-:: Define `telemac-debug` function
-@REM [[ $(type -t telemac-debug) == function ]] && unset -f telemac-debug
-
-telemac-debug ()
-{
-    if [%1]==[on] (
-        set USETELCFG=gnu.dynamic.debug
-        echo Telemac debug mode ON
-    )
-    if [%1]==[off] (
-        set USETELCFG=gnu.dynamic
-        echo Telemac debug mode OFF
-    )
-}
